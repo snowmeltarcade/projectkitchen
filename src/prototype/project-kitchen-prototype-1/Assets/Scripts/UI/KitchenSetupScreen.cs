c407df1a9909108056e02cfce9437e9c168f6fdf
Richard Hughes <richard.j.hughes@hotmail.com>
2022-07-23 20:48:58 -0400

    Start setting required and available workstations in the kitchen setup screen.

--- src/prototype/project-kitchen-prototype-1/Assets/Scripts/UI/KitchenSetupScreen.cs
+++ src/prototype/project-kitchen-prototype-1/Assets/Scripts/UI/KitchenSetupScreen.cs
@@ -19,6 +19,66 @@ namespace SnowMeltArcade.ProjectKitchen.UI
             }
 
             buttonDone.RegisterCallback<ClickEvent>(evt => { this.UIController.ShowKitchenLobbyScreen(); });
+
+            var workstationSlotTemplate = Resources.Load<VisualTreeAsset>("WorkstationSlot");
+            if (workstationSlotTemplate is null)
+            {
+                Debug.LogError("Failed to find template `WorkstationSlot`.");
+                return;
+            }
+            
+            var workstationTemplate = Resources.Load<VisualTreeAsset>("Workstation");
+            if (workstationTemplate is null)
+            {
+                Debug.LogError("Failed to find template `Workstation`.");
+                return;
+            }
+            
+            this.SetupWorkstations(workstationSlotTemplate, workstationTemplate);
+            
+            this.SetupAvailableWorkstations(workstationSlotTemplate, workstationTemplate);
+        }
+
+        private void SetupWorkstations(VisualTreeAsset workstationSlotTemplate, VisualTreeAsset workstationTemplate)
+        {
+            var requiredWorkstations = this.UIDocument.rootVisualElement.Q<VisualElement>("requiredWorkstations");
+            if (requiredWorkstations is null)
+            {
+                Debug.LogError("Failed to find element `requiredWorkstations`.");
+                return;
+            }
+            
+            requiredWorkstations.Clear();
+
+            for (var i = 0; i < 4; ++i)
+            {
+                var slot = workstationSlotTemplate.Instantiate();
+                var workstation = workstationTemplate.Instantiate();
+                
+                slot.Add(workstation);
+                requiredWorkstations.Add(slot);
+            }
+        }
+
+        private void SetupAvailableWorkstations(VisualTreeAsset workstationSlotTemplate, VisualTreeAsset workstationTemplate)
+        {
+            var availableWorkstations = this.UIDocument.rootVisualElement.Q<VisualElement>("availableWorkstations");
+            if (availableWorkstations is null)
+            {
+                Debug.LogError("Failed to find element `availableWorkstations`.");
+                return;
+            }
+            
+            availableWorkstations.Clear();
+
+            for (var i = 0; i < 10; ++i)
+            {
+                var slot = workstationSlotTemplate.Instantiate();
+                var workstation = workstationTemplate.Instantiate();
+                
+                slot.Add(workstation);
+                availableWorkstations.Add(slot);
+            }
         }
     }
 }
